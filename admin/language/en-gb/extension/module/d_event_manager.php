88ce66f1f1fb3413272cf419e35278192a9f8baf
dmitriyzhuk <dmitriyzhuk@gmail.com>
2017-07-31 01:43:52 +0300

    fix

--- admin/language/en-gb/extension/module/d_event_manager.php
+++ admin/language/en-gb/extension/module/d_event_manager.php
@@ -76,7 +76,7 @@ $_['success_no_update']         = 'Super! You have the latest version.';
 $_['warning_new_update']        = 'Wow! There is a new version available for download.';
 $_['error_update']              = 'Sorry! Something went wrong. If this repeats, contact the support please.';
 $_['error_failed']              = 'Oops! We could not connect to the server. Please try again later.';
-$_['success_twig_compatible']   = 'Twig installed. You can not go to Event Manager';
+$_['success_twig_compatible']   = 'Twig installed. You can now go to Event Manager';
 //debug
 $_['tab_debug']                 = 'Debug';
 $_['entry_debug']               = 'Debug';
