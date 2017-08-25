83df821e6fe5c95ff19a53ea3ceadafee0551cfb
dmitriyzhuk <dmitriyzhuk@gmail.com>
2017-08-26 02:10:52 +0300

    fix

--- admin/controller/extension/module/d_event_manager.php
+++ admin/controller/extension/module/d_event_manager.php
@@ -68,6 +68,10 @@ class ControllerExtensionModuleDEventManager extends Controller {
 
             if(VERSION < '3.0.0.0'){
                 $this->request->post[$this->codename.'_setting']['skipped_models'] = explode(",", $this->request->post[$this->codename.'_setting']['skipped_models']);
+
+                if(empty($this->request->post[$this->codename.'_setting']['skipped_models'])){
+                    $this->request->post[$this->codename.'_setting']['skipped_models'][] = 'total';
+                }
             }
             $this->model_setting_setting->editSetting($this->codename, $this->request->post, $this->store_id);
             $this->session->data['success'] = $this->language->get('text_success');
