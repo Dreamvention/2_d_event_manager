b8d776321ca8f00053fab4724c6bfd362ffceefc
dmitriyzhuk <dmitriyzhuk@gmail.com>
2018-02-16 13:50:30 +0200

    v1.3.5 Fix for old php versions

--- system/library/d_event_manager/event.php
+++ system/library/d_event_manager/event.php
@@ -8,76 +8,76 @@
 namespace d_event_manager;
 
 class Event {
-	protected $registry;
-	protected $data = array();
+    protected $registry;
+    protected $data = array();
 
-	public function __construct($registry) {
-		
-		if(VERSION < '2.2.0.0'){
-			$db = new \DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-			$registry->set('db', $db);
-		}
-
-		$this->registry = $registry;
-		$this->register_all();
-	}
-
-	public function register_all(){
-		if(VERSION < '2.2.0.0'){
-			$location = 'catalog';
-			if(defined('HTTP_CATALOG')){
-				$location = 'admin';
-			}
-			
-			$query = $this->registry->get('db')->query("SELECT * FROM `" . DB_PREFIX . "event` WHERE `trigger` LIKE '".$location."/%' AND `status` = '1' ORDER BY `event_id` ASC");
-			
-			foreach ($query->rows as $result) {
-				$this->register(substr($result['trigger'], strpos($result['trigger'], '/') + 1), new Action($result['action']));
-			}
-		}
-	}
-
-	public function register($trigger, Action $action) {
-		$this->data[$trigger][] = $action;
-	}
-	
-	public function trigger($event, array $args = array()) {
+    public function __construct($registry) {
         
-		foreach ($this->data as $trigger => $actions) {
+        if(VERSION < '2.2.0.0'){
+            $db = new \DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+            $registry->set('db', $db);
+        }
+
+        $this->registry = $registry;
+        $this->register_all();
+    }
+
+    public function register_all(){
+        if(VERSION < '2.2.0.0'){
+            $location = 'catalog';
+            if(defined('HTTP_CATALOG')){
+                $location = 'admin';
+            }
+            
+            $query = $this->registry->get('db')->query("SELECT * FROM `" . DB_PREFIX . "event` WHERE `trigger` LIKE '".$location."/%' AND `status` = '1' ORDER BY `event_id` ASC");
+            
+            foreach ($query->rows as $result) {
+                $this->register(substr($result['trigger'], strpos($result['trigger'], '/') + 1), new Action($result['action']));
+            }
+        }
+    }
+
+    public function register($trigger, Action $action) {
+        $this->data[$trigger][] = $action;
+    }
+    
+    public function trigger($event, array $args = array()) {
+        
+        foreach ($this->data as $trigger => $actions) {
 
             if(preg_match('/view\/[^*][^theme]/', $trigger) && !defined('HTTP_CATALOG') && strpos($trigger, 'before') == false){
                 $trigger = str_replace('view/', 'view/*/', $trigger);
             }
 
-			if (preg_match('/^' . str_replace(array('\*', '\?'), array('.*', '.'), preg_quote($trigger, '/')) . '/', $event)) {
-				foreach ($actions as $action) {
-					$result = $action->execute($this->registry, $args);
+            if (preg_match('/^' . str_replace(array('\*', '\?'), array('.*', '.'), preg_quote($trigger, '/')) . '/', $event)) {
+                foreach ($actions as $action) {
+                    $result = $action->execute($this->registry, $args);
 
-					if (!is_null($result) && !($result instanceof Exception)) {
-						return $result;
-					}
-				}
-			}
-		}
-	}
+                    if (!is_null($result) && !($result instanceof Exception)) {
+                        return $result;
+                    }
+                }
+            }
+        }
+    }
 
-	public function unregister($trigger, $route = '') {
-		if ($route) {
-			foreach ($this->data[$trigger] as $key => $action) {
-				if ($action->getId() == $route) {
-					unset($this->data[$trigger][$key]);
-				}
-			}
-		} else {
-			unset($this->data[$trigger]);
-		}
-	}
+    public function unregister($trigger, $route = '') {
+        if ($route) {
+            foreach ($this->data[$trigger] as $key => $action) {
+                if ($action->getId() == $route) {
+                    unset($this->data[$trigger][$key]);
+                }
+            }
+        } else {
+            unset($this->data[$trigger]);
+        }
+    }
 
-	public function removeAction($trigger, $route) {
-		foreach ($this->data[$trigger] as $key => $action) {
-			if ($action->getId() == $route) {
-				unset($this->data[$trigger][$key]);
-			}
-		}
-	}
+    public function removeAction($trigger, $route) {
+        foreach ($this->data[$trigger] as $key => $action) {
+            if ($action->getId() == $route) {
+                unset($this->data[$trigger][$key]);
+            }
+        }
+    }
 }
--- system/library/d_event_manager/loader.php
+++ system/library/d_event_manager/loader.php
@@ -2,193 +2,195 @@
 namespace d_event_manager;
 
 final class Loader {
-	protected $class;
+    protected $class;
 
 
-	public function __construct($class, $registry) {
-		$this->class = $class;
-		$this->registry = $registry;
-		$this->event = new Event($registry);
-	}
-	public function controller($route, $data = array()){
+    public function __construct($class, $registry) {
+        $this->class = $class;
+        $this->registry = $registry;
+        $this->event = new Event($registry);
+    }
+    public function controller($route, $data = array()){
 
-		if(VERSION < '2.2.0.0'){
-			$route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$route);
-			
-			$output = null;
-			
-			// Trigger the pre events
-			$result = $this->event->trigger('controller/' . $route . '/before', array(&$route, &$data, &$output));
-			
+        if(VERSION < '2.2.0.0'){
+            $route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$route);
+            
+            $output = null;
+            
+            // Trigger the pre events
+            $result = $this->event->trigger('controller/' . $route . '/before', array(&$route, &$data, &$output));
+            
 
-			$output = $this->class->_controller($route, $data);
+            $output = $this->class->_controller($route, $data);
 
-			// Trigger the post events
-			$result = $this->event->trigger('controller/' . $route . '/after', array(&$route, &$data, &$output));
-			
-		}else{
-			$output = $this->class->_controller($route, $data);
-		}
+            // Trigger the post events
+            $result = $this->event->trigger('controller/' . $route . '/after', array(&$route, &$data, &$output));
+            
+        }else{
+            $output = $this->class->_controller($route, $data);
+        }
 
-		if ($output instanceof Exception) {
-			return false;
-		}
+        if ($output instanceof Exception) {
+            return false;
+        }
 
-		return $output;
-	}
+        return $output;
+    }
 
-	public function model($route, $data = array()){
-		if(VERSION < '2.2.0.0'){
-			$route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$route);
-			
-			$output = null;
-			
-			// Trigger the pre events
-			$result = $this->event->trigger('model/' . $route . '/before', array(&$route));
+    public function model($route, $data = array()){
+        if(VERSION < '2.2.0.0'){
+            $route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$route);
+            
+            $output = null;
+            
+            // Trigger the pre events
+            $result = $this->event->trigger('model/' . $route . '/before', array(&$route));
 
-			//, $data = array()???
-			$output = $this->class->_model($route);
+            //, $data = array()???
+            $output = $this->class->_model($route);
 
-			// Trigger the post events
-			$result = $this->event->trigger('model/' . $route . '/after', array(&$route));
-		}else{
-			$output = $this->class->_model($route);
-		}
+            // Trigger the post events
+            $result = $this->event->trigger('model/' . $route . '/after', array(&$route));
+        }else{
+            $output = $this->class->_model($route);
+        }
 
-		if ($output instanceof Exception) {
-			return false;
-		}
+        if ($output instanceof Exception) {
+            return false;
+        }
 
-		return $output;
-	}
-	
-	public function view($template, $data = array()){
-		if(VERSION < '2.2.0.0'){
-			$output = null;
-			
-			// Sanitize the call
-			$route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$template);
-			//remove tpl
-			if (substr($route, -3) == 'tpl') {
-				$route = substr($route, 0, -3);
-			}
-			//remove twig
-			if (substr($route, -4) == 'twig') {
-				$route = substr($route, 0, -4);
-			}
+        return $output;
+    }
+    
+    public function view($template, $data = array()){
+        if(VERSION < '2.2.0.0'){
+            $output = null;
+            
+            // Sanitize the call
+            $route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$template);
+            //remove tpl
+            if (substr($route, -3) == 'tpl') {
+                $route = substr($route, 0, -3);
+            }
+            //remove twig
+            if (substr($route, -4) == 'twig') {
+                $route = substr($route, 0, -4);
+            }
 
-			$parts = explode('template/', $route);
-			if(isset($parts['1'])){
-				$route = $parts['1'];
-			}
+            $parts = explode('template/', $route);
+            if(isset($parts['1'])){
+                $route = $parts['1'];
+            }
 
-			// Trigger the pre events
-			$result = $this->event->trigger('view/' . $route . '/before', array(&$route, &$data, &$output));
+            // Trigger the pre events
+            $result = $this->event->trigger('view/' . $route . '/before', array(&$route, &$data, &$output));
 
-			if(!$output){
-				//, $data = array()???
-				$output = $this->class->_view($template, $data);
-			}
+            if(!$output){
+                //, $data = array()???
+                $output = $this->class->_view($template, $data);
+            }
 
-			$route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$template);
-			if (substr($route, -3) == 'tpl') {
-				$route = substr($route, 0, -3);
-			}
-			//remove twig
-			if (substr($route, -4) == 'twig') {
-				$route = substr($route, 0, -4);
-			}
+            $route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$template);
+            if (substr($route, -3) == 'tpl') {
+                $route = substr($route, 0, -3);
+            }
+            //remove twig
+            if (substr($route, -4) == 'twig') {
+                $route = substr($route, 0, -4);
+            }
 
-			// Trigger the post events
-			$result = $this->event->trigger('view/' . $route . '/after', array(&$route, &$data, &$output));
-		}else{
-			$output = $this->class->_view($template, $data);
-		}
+            // Trigger the post events
+            $result = $this->event->trigger('view/' . $route . '/after', array(&$route, &$data, &$output));
+        }else{
+            $output = $this->class->_view($template, $data);
+        }
 
-		if ($result) {
-			return $result;
-		}
-		
-		return $output;
-	}
+        if ($result) {
+            return $result;
+        }
+        
+        return $output;
+    }
 
-	
-	
-	public function config($route) {
+    
+    
+    public function config($route) {
 
-		if(VERSION < '2.2.0.0'){
-			$this->event->trigger('config/' . $route . '/before', array(&$route));
-			
-			$output = $this->class->_config($route);
-			
-			$this->event->trigger('config/' . $route . '/after', array(&$route, &$output));
-		}else{
-			$output = $this->class->_config($route);
-		}
-	}
+        if(VERSION < '2.2.0.0'){
+            $this->event->trigger('config/' . $route . '/before', array(&$route));
+            
+            $output = $this->class->_config($route);
+            
+            $this->event->trigger('config/' . $route . '/after', array(&$route, &$output));
+        }else{
+            $output = $this->class->_config($route);
+        }
+    }
 
-	public function language($route) {
-		if(VERSION < '2.2.0.0'){
-			$output = null;
-			
-			$this->event->trigger('language/' . $route . '/before', array(&$route, &$output));
-			
-			$output = $this->class->_language($route);
-			
-			$this->event->trigger('language/' . $route . '/after', array(&$route, &$output));
-		}else{
-			$output = $this->class->_language($route);
-		}
-		return $output;
-	}
-	
-	public function callback($registry, $route) {
-		return function($args) use($registry, &$route) {
-			static $model = array(); 			
-			
-			$output = null;
-			
-			// Trigger the pre events
-			$result = $this->event->trigger('model/' . $route . '/before', array(&$route, &$args, &$output));
-			
-			if ($result) {
-				return $result;
-			}
-			
-			// Store the model object
-			if (!isset($model[$route])) {
-				$file = DIR_APPLICATION . 'model/' .  substr($route, 0, strrpos($route, '/')) . '.php';
-				$class = 'Model' . preg_replace('/[^a-zA-Z0-9]/', '', substr($route, 0, strrpos($route, '/')));
+    public function language($route) {
+        if(VERSION < '2.2.0.0'){
+            $output = null;
+            
+            $this->event->trigger('language/' . $route . '/before', array(&$route, &$output));
+            
+            $output = $this->class->_language($route);
+            
+            $this->event->trigger('language/' . $route . '/after', array(&$route, &$output));
+        }else{
+            $output = $this->class->_language($route);
+        }
+        return $output;
+    }
+    
+    public function callback($registry, $route) {
+        
+        $self = $this;
+        return function($args) use($registry, &$route, $self) {
+            static $model = array();            
+            
+            $output = null;
+            
+            // Trigger the pre events
+            $result = $self->event->trigger('model/' . $route . '/before', array(&$route, &$args, &$output));
+            
+            if ($result) {
+                return $result;
+            }
+            
+            // Store the model object
+            if (!isset($model[$route])) {
+                $file = DIR_APPLICATION . 'model/' .  substr($route, 0, strrpos($route, '/')) . '.php';
+                $class = 'Model' . preg_replace('/[^a-zA-Z0-9]/', '', substr($route, 0, strrpos($route, '/')));
 
-				if (is_file($file)) {
+                if (is_file($file)) {
                     if(!class_exists($class)) {
                         include_once($file);
                     }
-				
-					$model[$route] = new $class($registry);
-				} else {
-					throw new \Exception('Error: Could not load model ' . substr($route, 0, strrpos($route, '/')) . '!');
-				}
-			}
+                
+                    $model[$route] = new $class($registry);
+                } else {
+                    throw new \Exception('Error: Could not load model ' . substr($route, 0, strrpos($route, '/')) . '!');
+                }
+            }
 
-			$method = substr($route, strrpos($route, '/') + 1);
-			
-			$callable = array($model[$route], $method);
+            $method = substr($route, strrpos($route, '/') + 1);
+            
+            $callable = array($model[$route], $method);
 
-			if (is_callable($callable)) {
-				$output = call_user_func_array($callable, $args);
-			} else {
-				throw new \Exception('Error: Could not call model/' . $route . '!');
-			}
-			
-			// Trigger the post events
-			$result = $this->event->trigger('model/' . $route . '/after', array(&$route, &$args, &$output));
-			
-			if ($result) {
-				return $result;
-			}
-						
-			return $output;
-		};
-	}	
+            if (is_callable($callable)) {
+                $output = call_user_func_array($callable, $args);
+            } else {
+                throw new \Exception('Error: Could not call model/' . $route . '!');
+            }
+            
+            // Trigger the post events
+            $result = $self->event->trigger('model/' . $route . '/after', array(&$route, &$args, &$output));
+            
+            if ($result) {
+                return $result;
+            }
+                        
+            return $output;
+        };
+    }   
 }
